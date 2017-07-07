3546d98a8a9291cfa87e105b14d438e8f83af266
Luca Garbolino <luca@aroundmeapp.com>
2017-07-07 10:59:56 +0200

    first commit

---
+++ SynchronizedArray/SynchronizedArray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SynchronizedArray
+//
+//  Created by Luca on 07/07/2017.
+//  Copyright © 2017 lg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SynchronizedArray/SynchronizedArrayTests/SynchronizedArrayTests.swift
@@ -0,0 +1,36 @@
+//
+//  SynchronizedArrayTests.swift
+//  SynchronizedArrayTests
+//
+//  Created by Luca on 07/07/2017.
+//  Copyright © 2017 lg. All rights reserved.
+//
+
+import XCTest
+@testable import SynchronizedArray
+
+class SynchronizedArrayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
